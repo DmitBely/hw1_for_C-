2f6faf345a149bd0100ae63e761dfc3854814388
dmitriy <dmitbely@protonmail.ch>
2022-08-21 15:10:46 +0300

    task008

---
+++ task008/Program.cs
@@ -0,0 +1,12 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.Write("Введите трёхзначное: ");
+int number = int.Parse(Console.ReadLine());
+
+int firtstN = number / 100;
+int secondN = (number % 100) / 10;
+int thirdN = number % 10;
+
+int newNumber = int.Parse(firtstN.ToString() + thirdN.ToString());
+
+Console.WriteLine($"Новое число это {newNumber}");
