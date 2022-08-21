bc108a73116141996f996ec1dc4a6fc73b35b7b8
dmitriy <dmitbely@protonmail.ch>
2022-08-21 14:29:09 +0300

    task005

---
+++ task005/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать четные числа от 1 до N
+
+Console.Write("Введите число до которого нужно вывести чётные: ");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 2; i <= number; i += 2) Console.WriteLine(i);
