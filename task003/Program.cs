5aec089e533747eb326033f28abf36bb83d4ff1a
dmitriy <dmitbely@protonmail.ch>
2022-08-21 14:02:39 +0300

    task003

---
+++ task003/Program.cs
@@ -0,0 +1,26 @@
+﻿// Найти максимальное из трех чисел
+
+Console.Write("Введите последовательно три числа: ");
+int first = int.Parse(Console.ReadLine());
+Console.Write("Теперь второе: ");
+int second = int.Parse(Console.ReadLine());
+Console.Write("Теперь третье: ");
+int third = int.Parse(Console.ReadLine());
+
+int max = first;
+
+if (second >= max)
+{
+    max = second;
+}
+else
+{
+    max = first;
+}
+
+if (third >= max)
+{
+    max = third;
+}
+
+Console.WriteLine($"Максимальное число = {max}");
