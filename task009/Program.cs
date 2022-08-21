274fe7c7990ab4c8aaa2a5c20d87f5a3c3d5629d
dmitriy <dmitbely@protonmail.ch>
2022-08-21 15:35:11 +0300

    task009

---
+++ task009/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.Write("Задайте число: ");
+int numberToCheck = int.Parse(Console.ReadLine());
+Console.Write("Кратность какому числу нужно проверить: ");
+int checkingNumber = int.Parse(Console.ReadLine());
+
+if (numberToCheck % checkingNumber == 0)
+{
+    Console.WriteLine($"Число {numberToCheck} - кратно числу {checkingNumber} ");
+}
+else
+{
+    Console.WriteLine($"Число {numberToCheck} - не кратно числу {checkingNumber} ");    
+}
