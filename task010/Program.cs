6c8a3046b8e2d62fd48d89059cc9a69e6c213bff
dmitriy <dmitbely@protonmail.ch>
2022-08-21 15:58:12 +0300

    task010

---
+++ task010/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+string stringed = number.ToString();
+
+Console.WriteLine(stringed);
+
+if (stringed[2] != null)
+{
+    Console.WriteLine($"Третий символ {stringed[2]}");    
+}
+else
+{
+    Console.WriteLine("Третьего символа нет"); 
+}
