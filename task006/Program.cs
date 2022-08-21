648823762821c9ea97e2f2c7e05d6807da1e8b0f
dmitriy <dmitbely@protonmail.ch>
2022-08-21 14:36:12 +0300

    task006

---
+++ task006/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.Write("Введите трёхзначное число ");
+int number = int.Parse(Console.ReadLine());
+
+int step1 = number % 100;
+int step2 = step1 / 10;
+
+Console.Write($"Вторая цифра трёхзначного числа это  {step2}");
